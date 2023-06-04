1d2ab5ddedd9420b33c292269ef5a68a1868634d
horcocks <134341370+horcocks@users.noreply.github.com>
2023-06-04 10:11:00 -0700

    Update Character_Chat_Harmony.py

--- Character_Chat_Harmony.py
+++ Character_Chat_Harmony.py
@@ -8,7 +8,7 @@ load_dotenv()
 anthropic_client = anthropic.Client(api_key=input("API KEY: "))
 context = "with the creation of original characters and the revision of existing characters, for the purpose of creative writing. When provided an existing character, give a detailed criticism of the character including both positive and negative points when relevant. When discussing specific characters, always end your messages with the character's updated ComCard. A ComCard is a format designed to convey characterization as efficiently as possible. ComCards are formatted like[ Character Name[ (Gender) (In-universe descriptor 1) (In-universe descriptor 2 if needed) 2-3 Physical Description tags 4-8 Personality tags, with at least one “negative” trait. 2-4 Secondary appearance tags, such as clothing or body language In-universe special characteristics. Backstory Motivations Strengths Flaws Intercharacter Relationships] When asked to change a character, update their ComCard in addition to your normal reply."
 while True:
-    user_inp = input("I am Eithne, the Muse of Inkwells. Show me your story, and I shall write you a masterpiece.: ")
+    user_inp = input("I am Harmony, the Muse of Characterization. Tell me about your character, and together we will make them complete.: ")
 
     current_inp = anthropic.HUMAN_PROMPT + user_inp + anthropic.AI_PROMPT
     context += current_inp
